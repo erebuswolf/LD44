eb5a6d4a39a2b40a0e73b5ea0cf4203d1cd3d1c2
Jesse Fish <jesfish@gmail.com>
2019-04-27 01:28:59 -0700

    Camera fucking works.

---
+++ Assets/Simple2DFollowCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simple2DFollowCamera : MonoBehaviour {
+
+    public float speed = 15f;
+    public float minDistanceX;
+    public float minDistanceY;
+    
+    public GameObject target;
+    public Vector3 offset;
+
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    void Start() {
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (target) {
+            Vector3 posNoZ = transform.position + offset;
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+            bool noXVal = Mathf.Abs(targetDirection.x) < minDistanceX;
+            bool noYVal = Mathf.Abs( targetDirection.y) < minDistanceY;
+            float interpVelocity = targetDirection.magnitude * speed;
+            Vector3 applyMask = new Vector3(noXVal ? 0 : 1, noYVal ? 0 : 1, 1);
+
+            applyMask.Scale(targetDirection.normalized * interpVelocity * Time.deltaTime);
+            targetPos = (transform.position) + applyMask;
+
+            float lerpAmount = 0.25f;
+            
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpAmount);
+
+        }
+    }
+}
